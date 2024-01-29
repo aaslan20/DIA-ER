1c893052aaab98c961d67b4e2dccde7295089a5d
Silvio Echsle <s.echsle@web.de>
2024-01-29 18:04:53 +0100

    Merging

--- spark/matchers_spark.py
+++ spark/matchers_spark.py
@@ -6,6 +6,9 @@ from pyspark.sql.functions import col
 from pyspark.sql.types import StringType
 from pyspark.sql import DataFrame
 from Levenshtein import ratio
+import time
+from itertools import product
+import pyspark.sql.functions as F
 
 group_years = udf(lambda year, year_block, labels: labels[len([y for y in year_block if y <= year])-1], StringType())
 
@@ -23,3 +26,79 @@ def apply_similarity_sorted(blocks1: DataFrame, blocks2: DataFrame, threshold: f
     matched_blocks_list = [(row["blocks1.index"], row["blocks2.index"]) for row in matched_blocks.collect()]
 
     return matched_blocks_list
+
+def apply_similarity_blocks_spark(df1, df2, threshold, similarity_function):
+    start_time = time.time()   
+    similarity_udf = F.udf(lambda set1, set2: str(similarity_function(set(set1), set(set2))), StringType())
+
+    joined_blocks = df1.alias("block1").crossJoin(df2.alias("block2"))
+    similar_pairs_df = joined_blocks.where(
+        (F.col("block1.value") == F.col("block2.value"))
+    ).select(
+        F.col("block1.index").alias("index1"),
+        F.col("block2.index").alias("index2"),
+        similarity_udf(F.col("block1.value"), F.col("block2.value")).alias("similarity")
+    )
+
+    similar_pairs_df = similar_pairs_df.filter(F.col("similarity") >= threshold)
+    similar_pairs = similar_pairs_df.collect()
+
+    processed_data = []
+   
+    for row in similar_pairs:
+        index1_values = row['index1'][1:-1].split(', ')
+        index2_values = row['index2'][1:-1].split(', ')
+
+        if len(index1_values) > 1 or len(index2_values) > 1:
+            index_combinations = list(product(index1_values, index2_values))
+            for combination in index_combinations:
+                processed_data.append((combination[0], combination[1]))
+        else:
+            
+            processed_data.append(row)
+            processed_data = list(set(processed_data))
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Processing time: {elapsed_time} seconds. Number of index combinations: {len(processed_data)}")
+
+    return processed_data
+# for ngram_blocking cause is a list not a dict (similiar_pairs)
+def apply_ngram_blocks_spark(df1, df2, threshold, similarity_function):
+    start_time = time.time()
+ 
+    similarity_udf = F.udf(lambda set1, set2: str(similarity_function(set(set1), set(set2))), StringType())
+
+    joined_blocks = df1.alias("block1").crossJoin(df2.alias("block2"))
+    similar_pairs_df = joined_blocks.where(
+        (F.col("block1.value") == F.col("block2.value"))
+    ).select(
+        F.col("block1.index").alias("index1"),
+        F.col("block2.index").alias("index2"),
+        similarity_udf(F.col("block1.value"), F.col("block2.value")).alias("similarity")
+    )
+
+    similar_pairs_df = similar_pairs_df.filter(F.col("similarity") >= threshold)
+    similar_pairs = similar_pairs_df.collect()
+
+    processed_data = []
+   
+    for row in similar_pairs:
+   
+        index1_values = row.index1
+        index2_values = row.index2
+        index1_values = [value for value in index1_values]
+        index2_values = [value for value in index2_values]
+
+        if len(index1_values) > 1 or len(index2_values) > 1:
+            index_combinations = list(product(index1_values, index2_values))
+            for combination in index_combinations:
+                processed_data.append((combination[0], combination[1]))
+        else:
+            processed_data.append(row)
+        
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Processing time: {elapsed_time} seconds. Number of index combinations: {len(processed_data)}")
+
+    return processed_data
